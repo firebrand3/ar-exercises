55e7a5e92d64231e72d093e2fcc8d93f86763176
Asim <asim.awan@outlook.com>
2022-01-01 04:06:44 +0000

    Get user input and validate rules

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name: "
+
+store_name = gets.chomp
+
+@store_new = Store.create(name: store_name)
+
+if !@store_new.valid?
+error_messages = @store_new.errors.full_messages
+end
+
+error_messages.each do |error|
+  puts "Error: #{error}"
+end
