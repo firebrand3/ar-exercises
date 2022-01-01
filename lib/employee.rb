e4f1b0f2ad14ec33a4471b756509b72e11333c28
Asim <asim.awan@outlook.com>
2022-01-01 04:08:11 +0000

    Add validations to enforce business rules

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates_presence_of :first_name, :last_name
+  validates_numericality_of :hourly_rate, in: 40..200
+  validates_associated :store
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates_length_of :name, :minimum =>3
+  validates_numericality_of :annual_revenue, greater_than_or_equal_to: 0
+  validate :must_carry_at_least_one_of_men_or_women_apparel
+
+  def must_carry_at_least_one_of_men_or_women_apparel
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, "must have at least one men or women apparel")
+    end
+  end
+
 end
